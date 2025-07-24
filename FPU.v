40c19238bc43a8960a292d7922442f56a50bd234
mk-1304 <161197773+mk-1304@users.noreply.github.com>
2025-07-24 16:37:01 +0530

    Update FPU.v

--- FPU.v
+++ FPU.v
@@ -1,1 +1,9 @@
-For code reach out via mail
+
+| Operation      |Opcode |
+|----------------|-----  |
+| Conversion     | 000   |
+| Addition       | 001   |
+| Subtraction    | 010   |
+| Multiplication | 011   |
+| Division       | 100   |
+
